b67e02c5833360fb49b095330192238e89bbc178
malithi19 <malithiranawaka8@gmail.com>
2024-05-19 10:49:51 +0530

    Enhanced bike.py with additional attributes and methods in the Bike class.

--- bike.py
+++ bike.py
@@ -1,4 +1,18 @@
 class Bike:
-	def __init__ (self, name, type):
+	def __init__ (self, name, bike_type):
 		self.name = name
-		self.type = type
+		self.type = bike_type
+		self.speed = 0
+	def accelerate(self, increment):
+        	self.speed += increment
+        	return self.speed
+
+    	def brake(self, decrement):
+        	self.speed -= decrement
+        	if self.speed < 0:
+            		self.speed = 0
+        	return self.speed
+
+    	def __str__(self):
+        	return f'Bike(name={self.name}, type={self.type}, speed={self.speed})'
+	
