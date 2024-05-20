8974cadc2543ac70f702682dd32f22581300a7ef
malithi19 <malithiranawaka8@gmail.com>
2024-05-20 23:09:51 +0530

    Update activity4.py

--- activity4.py
+++ activity4.py
@@ -1,22 +1,24 @@
 from car import Car
 from bike import Bike
+from truck import Truck
 
 car1 = Car('Mercedes')
 car2 = Car('Toyota')
-
 bike1 = Bike("Yamaha", "Sport")
-
-print(car1)
-print(car2)
-print(bike1)
+truck1 = Truck('Volvo', 15)  # 15 tons capacity
 
 # Demonstrate the new methods
 car1.drive(100)
 car2.drive(200)
 bike1.accelerate(50)
 bike1.brake(20)
+truck1.drive(300)
+print(truck1.load(10))  
+print(truck1.load(20))  
 
 print(car1)
 print(car2)
 print(bike1)
+print(truck1)
+
 
